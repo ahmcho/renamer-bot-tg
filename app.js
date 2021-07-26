0b034115ae8636f141a606cfb122d58b37a7631e
ahmcho <mail@ahmcho.com>
2021-07-26 11:26:38 +0400

    Removed debug console.log

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
-const { Telegraf, Markup } = require('telegraf')
+const {
+  Telegraf,
+  Markup
+} = require('telegraf')
 
 if (process.env.BOT_TOKEN === undefined) {
   throw new TypeError('BOT_TOKEN must be provided!')
@@ -14,18 +17,17 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Hello'))
 bot.help((ctx) => ctx.reply('Help message'))
 bot.on('channel_post', async (ctx) => {
-    if(ctx.update.channel_post.video){
-      let filename = ctx.update.channel_post.video.file_name.split('.')[0];
-      if(filename.match(/_/)){
-        filename = filename.replace(/_/gm,' ');
-      }
-      console.log(ctx.update.channel_post);
-      await ctx.deleteMessage(ctx.update.channel_post.message_id);
-      await ctx.replyWithVideo(ctx.update.channel_post.video.file_id, {
-          caption: `${filename}`
-      });
+  if (ctx.update.channel_post.video) {
+    let filename = ctx.update.channel_post.video.file_name.split('.')[0];
+    if (filename.match(/_/)) {
+      filename = filename.replace(/_/gm, ' ');
     }
-    
+    await ctx.deleteMessage(ctx.update.channel_post.message_id);
+    await ctx.replyWithVideo(ctx.update.channel_post.video.file_id, {
+      caption: `${filename}`
+    });
+  }
+
 });
 bot.action('delete', (ctx) => ctx.deleteMessage())
 bot.launch()
